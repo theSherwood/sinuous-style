c35fe678c4984b1a15bfcf7949adb4c3689fbb92
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-04-05 08:31:13 -0600

    Add output.globals to eliminate CDN config.

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,19 @@ import { terser } from "rollup-plugin-terser";
 
 export default [
   {
-    input: "./src/scopeApi.js",
+    input: "./src/index.js",
     output: [
       {
         file: "dist/min.js",
         name: "sinuousStyle",
         format: "iife",
+        globals: {
+          sinuous: "S",
+          ["sinuous/observable"]: "S",
+        },
         compact: true,
-        plugins: [terser()]
-      }
-    ]
-  }
+        plugins: [terser()],
+      },
+    ],
+  },
 ];
